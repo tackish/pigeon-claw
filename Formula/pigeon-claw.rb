835ede707e4e2c008e3ee7157f2ed06da3dd853b
Tek <tackish@nate.com>
2026-04-10 10:24:29 +0000

    brew: update formula to 0.0.26

--- Formula/pigeon-claw.rb
+++ Formula/pigeon-claw.rb
@@ -1,16 +1,16 @@
 class PigeonClaw < Formula
   desc "Discord-based remote Mac agent - lightweight alternative to openclaw"
   homepage "https://github.com/tackish/pigeon-claw"
-  version "0.0.25"
+  version "0.0.26"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/tackish/pigeon-claw/releases/download/v#{version}/pigeon-claw-darwin-arm64.tar.gz"
-      sha256 "0eee9a901bd2deee83a70b281cd5c86eaf0943dc2609aae3ffe25a8d3fe1dfd5"
+      sha256 "74d7cb78c7d39474575744d450ffb4f9e1c31551579cfedc44db331f0f9d2de7"
     else
       url "https://github.com/tackish/pigeon-claw/releases/download/v#{version}/pigeon-claw-darwin-amd64.tar.gz"
-      sha256 "6932cad898e649341ee962e3bf623f19297bf165ed104a734d72b37606bf3e8b"
+      sha256 "fa1da85f7e2e10f58a6f4ece79d3644a6440f02a710e82a2c47e5a7395166e2e"
     end
   end
 
